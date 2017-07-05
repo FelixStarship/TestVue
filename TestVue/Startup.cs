3b51cfa56740696279c22ce766db02b531277d91
xuze hu <2633185948@qq.com>
2017-07-05 10:57:22 +0800

    添加项目文件。

---
+++ TestVue/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestVue.Startup))]
+namespace TestVue
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
